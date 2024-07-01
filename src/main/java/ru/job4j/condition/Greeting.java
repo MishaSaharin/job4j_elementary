037d7dae1e2d05f1d2d044faaefbed31b11c6935
MishaSaharin <zakharin.mikhail@gmail.com>
2024-07-01 15:17:43 +0300

    changes class Greeting

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -3,9 +3,10 @@ package ru.job4j.condition;
 public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
-        int year = 2024;
-        System.out.println(idea + " But I am a newbie.");
-        idea = idea + year;
+        System.out.println(idea);
+        idea += " But I am a newbie. ";
+        int year = 2022;
+        idea += year;
         System.out.println(idea);
     }
 }
